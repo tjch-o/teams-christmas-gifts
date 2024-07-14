bf5d59318aba6247ce656f2383b7b1938f8c2038
choo tze jie <119966583+tjch-o@users.noreply.github.com>
2024-07-15 02:56:31 +0800

    Implement retrieval of team name

--- index.ts
+++ index.ts
@@ -1,22 +1,28 @@
-const express = require('express')
-const { PrismaClient } = require('@prisma/client')
+import express, { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 
-const app = express()
-const prisma = new PrismaClient()
+const app = express();
+const prisma = new PrismaClient();
+app.use(express.json());
 
-app.use(express.json())
+app.get("/staff/:id", async (req: Request, res: Response) => {
+    const { id } = req.params;
 
-// app.get("/test", async (req, res) => {
-//     try {
-//         const result = await prisma.staff.findMany()
-//         res.status(200).json(result)
-//     } catch (e) {
-//         console.log(e)
-//     }
-// })
-
+    try {
+        const result = await prisma.staff.findMany({
+            where: {
+                staff_pass_id: id,
+            },
+        });
+        res.status(200).json(result[0]);
+        console.log(`retrieved ${id} team name: ${result[0].team_name}`);
+    } catch (e) {
+        console.log(e);
+    }
+});
 
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
-    console.log(`server is running on port ${port}`)
-})
+    console.log(`server is running on port ${port}`);
+});
